e068454bd0e6da40be01771fb964c21506f55873
Christopher Kormanyos <ckormanyos@yahoo.com>
2020-08-09 15:52:40 +0200

    Do clean up of test_sqrt example

--- example/test_sqrt/test_sqrt.cpp
+++ example/test_sqrt/test_sqrt.cpp
@@ -5,6 +5,7 @@
 //          http://www.boost.org/LICENSE_1_0.txt)
 //
 
+#include <algorithm>
 #include <iomanip>
 #include <iostream>
 #include <limits>
@@ -18,95 +19,88 @@
 #include <boost/random.hpp>
 
 #include <math/wide_integer/generic_template_uintwide_t.h>
-
-template<typename UnsignedIntegralType>
-static std::string hexlexical_cast(const UnsignedIntegralType& u)
+namespace
 {
-  std::stringstream ss;
-
-  ss << std::hex << u;
-
-  return ss.str();
-}
-
-template<typename OtherLocalUintType,
-         typename OtherBoostUintType>
-static void get_equal_random_test_values_boost_and_local_n(OtherLocalUintType* u_local,
-                                                           OtherBoostUintType* u_boost,
-                                                           const std::size_t count)
-{
-  boost::mt11213b base_gen(std::clock());
-
-  using independent_bits_local_uint_engine_type =
-    boost::random::independent_bits_engine<boost::mt11213b, std::numeric_limits<OtherLocalUintType>::digits, OtherLocalUintType>;
-
-  independent_bits_local_uint_engine_type gen_local_uint_type(base_gen);
-
-  using other_local_uint_type = OtherLocalUintType;
-  using other_boost_uint_type = OtherBoostUintType;
-
-  for(std::size_t i = 0U; i < count; ++i)
+  template<typename UnsignedIntegralType>
+  static std::string hexlexical_cast(const UnsignedIntegralType& u)
   {
-    const other_local_uint_type a = gen_local_uint_type();
+    std::stringstream ss;
 
-    u_local[i] = a;
-    u_boost[i] = other_boost_uint_type("0x" + hexlexical_cast(a));
+    ss << std::hex << u;
+
+    return ss.str();
   }
-}
 
-constexpr std::size_t digits2() { return 1UL << 14U; }
-constexpr std::size_t size   () { return 1000U; }
+  template<typename OtherLocalUintType,
+           typename OtherBoostUintType>
+  static void get_equal_random_test_values_boost_and_local_n(OtherLocalUintType* u_local,
+                                                             OtherBoostUintType* u_boost,
+                                                             const std::size_t count)
+  {
+    using other_local_uint_type = OtherLocalUintType;
+    using other_boost_uint_type = OtherBoostUintType;
 
-using boost_uint_backend_type =
-    boost::multiprecision::cpp_int_backend<digits2()>;
+    using independent_bits_local_uint_engine_type =
+      boost::random::independent_bits_engine<boost::mt11213b,
+                                             std::numeric_limits<other_local_uint_type>::digits,
+                                             other_local_uint_type>;
 
-using boost_uint_type = boost::multiprecision::number<boost_uint_backend_type, boost::multiprecision::et_off>;
+    boost::mt11213b base_gen(std::clock());
 
-using local_limb_type = std::uint32_t;
+    independent_bits_local_uint_engine_type gen_local_uint_type(base_gen);
 
-using local_uint_type = wide_integer::generic_template::uintwide_t<digits2(), local_limb_type>;
+    for(std::size_t i = 0U; i < count; ++i)
+    {
+      const other_local_uint_type a = gen_local_uint_type();
 
-std::vector<local_uint_type> a_local;
-std::vector<boost_uint_type> a_boost;
-std::vector<local_uint_type> result_local;
-std::vector<boost_uint_type> result_boost;
+      u_local[i] = a;
+      u_boost[i] = other_boost_uint_type("0x" + hexlexical_cast(a));
+    }
+  }
 
-void initialize()
-{
-  a_local.clear();
-  a_boost.clear();
-
-  a_local.resize(size());
-  a_boost.resize(size());
-
-  result_local.clear();
-  result_boost.clear();
-
-  result_local.resize(size());
-  result_boost.resize(size());
-
-  get_equal_random_test_values_boost_and_local_n(a_local.data(), a_boost.data(), size());
+  constexpr std::size_t digits2() { return 1UL << 14U; }
+  constexpr std::size_t size   () { return 1000U; }
 }
 
 BOOST_AUTO_TEST_CASE(test_square_root_random_trials)
 {
-  initialize();
+  using boost_uint_backend_type =
+      boost::multiprecision::cpp_int_backend<digits2()>;
+
+  using boost_uint_type =
+    boost::multiprecision::number<boost_uint_backend_type, boost::multiprecision::et_off>;
+
+  using local_limb_type = std::uint32_t;
+
+  using local_uint_type = wide_integer::generic_template::uintwide_t<digits2(), local_limb_type>;
+
+  std::vector<local_uint_type> a_local     (size());
+  std::vector<local_uint_type> result_local(size());
+
+  std::vector<boost_uint_type> a_boost     (size());
+  std::vector<boost_uint_type> result_boost(size());
+
+  get_equal_random_test_values_boost_and_local_n(a_local.data(), a_boost.data(), size());
 
   const std::clock_t start_local = std::clock();
-  for(std::size_t i = 0U; i < size(); ++i)
-  {
-    result_local[i] = sqrt(a_local[i]);
-  }
-  const std::clock_t elapsed_local = std::clock() - start_local;
+  std::transform(a_local.cbegin(),
+                 a_local.cend(),
+                 result_local.begin(),
+                 [](const local_uint_type& a) -> local_uint_type
+                 {
+                   return sqrt(a);
+                 });
+  const double elapsed_local_s = double(std::clock() - start_local) / double(CLOCKS_PER_SEC);
 
   const std::clock_t start_boost = std::clock();
-  for(std::size_t i = 0U; i < size(); ++i)
-  {
-    result_boost[i] = sqrt(a_boost[i]);
-  }
-  const std::clock_t elapsed_boost = std::clock() - start_boost;
-
-  std::size_t number_of_trials_that_are_ok = 0U;
+  std::transform(a_boost.cbegin(),
+                 a_boost.cend(),
+                 result_boost.begin(),
+                 [](const boost_uint_type& a) -> boost_uint_type
+                 {
+                   return sqrt(a);
+                 });
+  const double elapsed_boost_s = double(std::clock() - start_boost) / double(CLOCKS_PER_SEC);
 
   for(std::size_t i = 0U; i < size(); ++i)
   {
@@ -115,9 +109,6 @@ BOOST_AUTO_TEST_CASE(test_square_root_random_trials)
 
     BOOST_CHECK(str_boost == str_local);
   }
-
-  const double elapsed_local_s = double(elapsed_local) / double(CLOCKS_PER_SEC);
-  const double elapsed_boost_s = double(elapsed_boost) / double(CLOCKS_PER_SEC);
 
   std::cout << "elapsed_local_s: " << std::dec << elapsed_local_s << std::endl;
   std::cout << "elapsed_boost_s: " << std::dec << elapsed_boost_s << std::endl;
